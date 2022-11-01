674191057bfa1c65bd525986b566c69e2ac91ee4
Ruslan Korniichuk <ruslan.korniichuk@gmail.com>
2022-11-01 18:35:50 +0100

    Add code for AWS Serverless Application Model (SAM) deployment

---
+++ aws-serverless-application-model/code/lambda_function.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Name: lambda_function
+# Description: Echo bot with python-telegram-bot
+# Version: 0.1a2
+# Owner: Ruslan Korniichuk
+
+import json
+
+from telegram.ext import Dispatcher, MessageHandler, Filters
+from telegram import Update, Bot
+
+
+TOKEN = "<TOKEN>"  # token from @BotFather for your Telegram bot
+
+bot = Bot(token=TOKEN)
+dispatcher = Dispatcher(bot, None, use_context=True)
+
+
+def echo_message(update, context):
+    chat_id = update.message.chat_id
+    chat_text = update.message.text
+
+    context.bot.send_message(chat_id=chat_id, text=chat_text)
+
+
+def lambda_handler(event, context):
+    dispatcher.add_handler(MessageHandler(Filters.text, echo_message))
+
+    try:
+        dispatcher.process_update(
+            Update.de_json(json.loads(event["body"]), bot)
+        )
+
+    except Exception as e:
+        print(e)
+        return {"statusCode": 500}
+
+    return {"statusCode": 200}
