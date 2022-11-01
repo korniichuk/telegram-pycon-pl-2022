e81ba8e33ede5a48f29bf96c63a41935a9e59b72
Ruslan Korniichuk <ruslan.korniichuk@gmail.com>
2022-11-01 09:54:29 +0100

    Improve readability

--- bot1.py
+++ bot1.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # Name: bot1
 # Description: "Hello, World!" with pyTelegramBotAPI
-# Version: 0.1a1
+# Version: 0.1a2
 # Owner: Ruslan Korniichuk
 
 import os
@@ -11,12 +11,12 @@ from dotenv import load_dotenv
 import telebot
 
 load_dotenv()
-TOKEN = os.getenv('TOKEN')
+TOKEN = os.getenv("TOKEN")
 
 bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=["start"])
 def start(message):
     bot.reply_to(message, "🔥 Hello, World!")
 
